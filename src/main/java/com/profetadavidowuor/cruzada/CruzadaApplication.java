ed4b0f803be1308090aad073052b68ecbaba5345
Jeyson Junior Ventura Aguilar <jeysonv.sis@gmail.com>
2022-07-27 11:02:41 -0500

    primer commit

---
+++ src/main/java/com/profetadavidowuor/cruzada/CruzadaApplication.java
@@ -0,0 +1,13 @@
+package com.profetadavidowuor.cruzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruzadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruzadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profetadavidowuor/cruzada/CruzadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profetadavidowuor.cruzada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruzadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
