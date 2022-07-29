23f0d3eca83835916274bdfe439db5307f49537e
Jeyson Ventura <jeysonv.sis@gmail.com>
2022-07-29 07:46:35 -0500

    formulario de registro de conferencia

--- src/main/java/com/profetadavidowuor/cruzada/web/PrincipalController.java
+++ src/main/java/com/profetadavidowuor/cruzada/web/PrincipalController.java
@@ -6,14 +6,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class PrincipalController {
 
     @Value("${spring.application.name}")
     String appName;
 
     @GetMapping("/")
-    public String homePage(Model model) {
+    public String inicioPage(Model model) {
         model.addAttribute("appName", appName);
         return "home";
+    }
+
+    @GetMapping("/registroconferencia")
+    public String registroConferenciaPage(Model model) {
+        model.addAttribute("appName", appName);
+        return "registroconferencia";
     }
 }
